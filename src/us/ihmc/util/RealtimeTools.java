99c5315c2fbc8be1358cf4513ebdf2d22a37a0cd
Jesper Smith <jsmith@ihmc.us>
2014-12-02 21:28:23 +0000

    NONE: Removed the unsafe. does not fixbamboo

--- src/us/ihmc/util/RealtimeTools.java
+++ src/us/ihmc/util/RealtimeTools.java
@@ -17,31 +17,8 @@
  */
 package us.ihmc.util;
 
-import java.lang.reflect.Field;
-
-import sun.misc.Unsafe;
-
 public class RealtimeTools
 {
-   
-   private static final Unsafe unsafe;
-   static
-   {      
-      try {
-         Field field = sun.misc.Unsafe.class.getDeclaredField("theUnsafe");
-         field.setAccessible(true);
-         unsafe = (sun.misc.Unsafe) field.get(null);
-      } catch (Exception e) {
-         throw new AssertionError(e);
-      }
-
-   }
-   
-   public static final Unsafe getUnsafe()
-   {
-      return unsafe;
-   }
-   
    public static final int nextPowerOfTwo(int v)
    {
       // Algorithm from http://graphics.stanford.edu/~seander/bithacks.html#RoundUpPowerOf2
@@ -53,16 +30,15 @@ public class RealtimeTools
       v |= v >> 16;
       return ++v;
    }
-   
+
    public static final int nextDivisibleByEight(int v)
    {
-      return (v/8 + 1) * 8;
-   }
-   
-   public static final int nextDivisibleBySixteen(int v)
-   {
-      return (v/16 + 1) * 16;
+      return (v / 8 + 1) * 8;
    }
 
-   
+   public static final int nextDivisibleBySixteen(int v)
+   {
+      return (v / 16 + 1) * 16;
+   }
+
 }
