182a6452bd0db85f5a55f5e2dd3577ff36fa4277
Duncan Calvert <dcalvert@ihmc.us>
2015-01-23 19:06:29 +0000

    GFE-172: Added feature to auto-generate all unit test suites. Also auto-generated all *DockerUnitTestSuites. Won't fixBamboo.

--- test/us/ihmc/realtime/IHMCRealtimeDockerTestSuite.java
+++ test/us/ihmc/realtime/IHMCRealtimeDockerTestSuite.java
@@ -1,21 +1,22 @@
 package us.ihmc.realtime;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+//import us.ihmc.utilities.code.unitTesting.JUnitTestSuiteGenerator;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses
 ({
    us.ihmc.realtime.concurrent.ConcurrentCopierTest.class,
-   us.ihmc.realtime.concurrent.ConcurrentRingBufferTest.class,
-   us.ihmc.realtime.TestCyclic.class,
-   us.ihmc.realtime.TestJoin.class,
-   us.ihmc.realtime.TestPriority.class
+   us.ihmc.realtime.concurrent.ConcurrentRingBufferTest.class
 })
 
 public class IHMCRealtimeDockerTestSuite
 {
    public static void main(String[] args)
    {
+      //JUnitTestSuiteGenerator.generateTestSuite(IHMCRealtimeDockerTestSuite.class);
    }
 }
+
