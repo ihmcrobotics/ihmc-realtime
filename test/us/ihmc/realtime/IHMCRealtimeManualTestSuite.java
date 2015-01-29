ac84c2742634b68a1832c8337c08815453ad915f
Duncan Calvert <dcalvert@ihmc.us>
2015-01-29 21:37:10 +0000

    GFE-172: Generated manual test suites. Won't fixBamboo.

--- test/us/ihmc/realtime/IHMCRealtimeManualTestSuite.java
+++ test/us/ihmc/realtime/IHMCRealtimeManualTestSuite.java
@@ -1,1 +1,20 @@
+package us.ihmc.realtime;
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+//import us.ihmc.utilities.code.unitTesting.JUnitTestSuiteGenerator;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+})
+
+public class IHMCRealtimeManualTestSuite
+{
+   public static void main(String[] args)
+   {
+      //JUnitTestSuiteGenerator.generateTestSuite(IHMCRealtimeManualTestSuite.class);
+   }
+}
 
