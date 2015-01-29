cce8bd75f5d4cd13745ca6bab03bc98f81ea812a
Duncan Calvert <dcalvert@ihmc.us>
2015-01-29 22:08:12 +0000

    GFE-172: Generated UI test suites. Won't fixBamboo.

--- test/us/ihmc/realtime/IHMCRealtimeUITestSuite.java
+++ test/us/ihmc/realtime/IHMCRealtimeUITestSuite.java
@@ -1,1 +1,20 @@
+package us.ihmc.realtime;
+
+import org.junit.runner.*;
+import org.junit.runners.*;
+
+//import us.ihmc.utilities.code.unitTesting.JUnitTestSuiteGenerator;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+})
+
+public class IHMCRealtimeUITestSuite
+{
+   public static void main(String[] args)
+   {
+      //JUnitTestSuiteGenerator.generateTestSuite(IHMCRealtimeUITestSuite.class);
+   }
+}
 
