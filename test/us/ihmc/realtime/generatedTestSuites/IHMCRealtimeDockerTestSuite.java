620169b92043edf64a1db7d47c18c75761bb1ee0
Duncan Calvert <dcalvert@ihmc.us>
2015-02-03 20:35:36 +0000

    GFE-172: Generated test suites now go in generatedTestSuites directory.

---
+++ test/us/ihmc/realtime/generatedTestSuites/IHMCRealtimeDockerTestSuite.java
@@ -0,0 +1,22 @@
+package us.ihmc.realtime.generatedTestSuites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+//import us.ihmc.utilities.code.unitTesting.runner.JUnitTestSuiteRunner;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses
+({
+   us.ihmc.realtime.concurrent.ConcurrentCopierTest.class,
+   us.ihmc.realtime.concurrent.ConcurrentRingBufferTest.class
+})
+
+public class IHMCRealtimeDockerTestSuite
+{
+   public static void main(String[] args)
+   {
+      //new JUnitTestSuiteRunner(IHMCRealtimeDockerTestSuite.class);
+   }
+}
+
