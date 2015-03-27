8534f987d6f9148f41a3dbe27a8270a7f001653b
Igor Kalkov <ikalkov@ihmc.us>
2015-03-27 22:46:22 +0000

    NONE: Extended cyclic test. Won't fixBamboo

--- test/us/ihmc/realtime/TestCyclic.java
+++ test/us/ihmc/realtime/TestCyclic.java
@@ -17,25 +17,39 @@
  */
 package us.ihmc.realtime;
 
+import us.ihmc.affinity.CPUTopology;
+import us.ihmc.affinity.Processor;
+
 public class TestCyclic
 {
    public static void main(String[] args) throws InterruptedException
    {
       final int periodInNS = 1000000;
-      final int iterations = 10000;
+      final int iterations = 60000;
+      final double duration = (double) periodInNS * (double) iterations / 1e9;
+
+      final boolean endless = (args.length > 0) && (args[0].equals("--endless"));
+      if (endless)
+      {
+         System.out.println("You can cancel this test by pressing Ctrl+C");
+      }
+      else
+      {
+         System.out.println("Iteration count: " + iterations);
+         System.out.println("Estimated duration: " + duration + " seconds");
+      }
 
       PriorityParameters priorityParameters = new PriorityParameters(99);
       PeriodicParameters periodicParameters = new PeriodicParameters(new MonotonicTime(0, periodInNS));
-      
+
       RealtimeThread periodicRealtimeThread = new RealtimeThread(priorityParameters, periodicParameters, null)
       {
-         @Override 
-         public void run()
+         private void perform(int run)
          {
             long previousTime = 0;
-            
-            long averageJitter = 0;
+            long avgJitter = 0;
             long maxJitter = 0;
+
             for(int i = -1; i < iterations; i++)
             {
                super.waitForNextPeriod();
@@ -44,26 +58,37 @@ public class TestCyclic
                   previousTime = System.nanoTime();
                   continue;
                }
+
                long newTime = System.nanoTime();
-               
                long jitter = Math.abs(newTime - previousTime - periodInNS);
-               averageJitter += jitter;
-               
-               if(jitter > maxJitter) 
-               {
-                  maxJitter = jitter;
-               }
+
+               if(jitter > maxJitter) { maxJitter = jitter; }
+               avgJitter += jitter;
+
                previousTime = newTime;
             }
-            
-            System.out.println("Average jitter: " + (((double) averageJitter)/((double)iterations))/1e6 + "ms");
-            System.out.println("Max jitter: " + ((double)maxJitter)/(1e6) + "ms");
+
+            final double usAvgJitter = (double) avgJitter / (double) iterations / 1e3;
+            final double usMaxJitter = (double) maxJitter / 1e3;
+
+            System.out.format("[%d] Jitter: avg = %.4f us, max = %.4f us%n", run, usAvgJitter, usMaxJitter);
+         }
+
+         @Override
+         public void run()
+         {
+            int run = 1;
+
+            do { perform(run++); }
+            while (endless);
          }
       };
 
-      RealtimeMemory.lock();
-      periodicRealtimeThread.start();
+      System.out.println("Pinning the periodic thread to processor 1");
+      Processor testProcessor = new CPUTopology().getPackage(0).getCore(1).getDefaultProcessor();
+      periodicRealtimeThread.setAffinity(testProcessor);
 
-      Thread.sleep(1000);
+      periodicRealtimeThread.start();
+      periodicRealtimeThread.join();
    }
 }
