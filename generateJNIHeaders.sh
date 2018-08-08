f8731bf0d7e47f807ebea012cb0a7b371a9770d4
Tiffany Zheng <qz3@williams.edu>
2018-08-08 11:28:28 -0500

    publish from stash

--- generateJNIHeaders.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-javah -classpath ./bin -o csrc/RealtimeNative.h us.ihmc.realtime.RealtimeNative
-javah -classpath ./bin -o csrc/Affinity.h us.ihmc.affinity.Affinity
-javah -classpath ./bin -o csrc/Scheduler.h us.ihmc.process.Scheduler
