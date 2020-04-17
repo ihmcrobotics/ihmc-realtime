f856637d5963f39abec9296d03e89f97f36f6086
Duncan Calvert <dcalvert@ihmc.us>
2020-04-17 00:54:56 -0500

    Upgrade Gradle scripts to KTS

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+   id("us.ihmc.ihmc-build") version "0.20.1"
+   id("us.ihmc.ihmc-ci") version "5.3"
+   id("us.ihmc.ihmc-cd") version "1.8"
+}
+
+ihmc {
+   group = "us.ihmc"
+   version = "1.3.0"
+   vcsUrl = "https://github.com/ihmcrobotics/ihmc-realtime"
+   openSource = true
+
+   configureDependencyResolution()
+   configurePublications()
+}
+
+mainDependencies {
+   api("us.ihmc:ihmc-native-library-loader:1.2.1")
+}
+
+app.entrypoint("ihmc-realtime", "us.ihmc.realtime.TestBarrierSchedulerCyclic")
+
+val appDirectory = "/home/shadylady/IHMCRealtime"
+
+tasks.create("deploy")
+{
+   dependsOn("installDist")
+
+   doLast {
+      remote.session("rt", "shadylady")
+      {
+         exec("mkdir -p $appDirectory")
+
+         exec("rm -rf $appDirectory/bin")
+         exec("rm -rf $appDirectory/lib")
+
+         put(file("build/install/ihmc-realtime/bin").toString(), "$appDirectory/bin")
+         put(file("build/install/ihmc-realtime/lib").toString(), "$appDirectory/lib")
+
+         exec("chmod +x $appDirectory/bin/ihmc-realtime")
+      }
+   }
+}
