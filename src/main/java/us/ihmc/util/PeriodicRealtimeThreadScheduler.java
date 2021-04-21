c9c2ad0b9f8bc3637ca73a0273a153be708523aa
nadiaocu <nadiaocu@NADIA-OCU>
2021-04-21 09:51:53 -0500

    Added option to set the affinity for periodic RT threads.

--- src/main/java/us/ihmc/util/PeriodicRealtimeThreadScheduler.java
+++ src/main/java/us/ihmc/util/PeriodicRealtimeThreadScheduler.java
@@ -2,6 +2,7 @@ package us.ihmc.util;
 
 import java.util.concurrent.TimeUnit;
 
+import us.ihmc.affinity.Processor;
 import us.ihmc.realtime.MonotonicTime;
 import us.ihmc.realtime.PeriodicParameters;
 import us.ihmc.realtime.PeriodicRealtimeThread;
@@ -11,45 +12,57 @@ import us.ihmc.realtime.PriorityParameters;
  * Realtime implementation of the periodic thread scheduler.
  * 
  * @author Jesper Smith
- *
  */
 public class PeriodicRealtimeThreadScheduler implements PeriodicThreadScheduler
 {
    private final String name;
    private final PriorityParameters priorityParameters;
+   private final Processor[] processors;
    private PeriodicRealtimeThread thread;
 
    public PeriodicRealtimeThreadScheduler(String name, int priority)
    {
       this(name, new PriorityParameters(priority));
    }
-   
+
    public PeriodicRealtimeThreadScheduler(int priority)
    {
       this(null, new PriorityParameters(priority));
    }
-   
+
    public PeriodicRealtimeThreadScheduler(PriorityParameters priorityParameters)
    {
       this(null, priorityParameters);
    }
-   
+
    public PeriodicRealtimeThreadScheduler(String name, PriorityParameters priorityParameters)
    {
       this.name = name;
       this.priorityParameters = priorityParameters;
+      this.processors = null;
+   }
+
+   public PeriodicRealtimeThreadScheduler(String name, PriorityParameters priorityParameters, Processor... processors)
+   {
+      this.name = name;
+      this.priorityParameters = priorityParameters;
+      this.processors = processors;
    }
 
    @Override
    public void schedule(Runnable runnable, long period, TimeUnit timeunit)
    {
-      if(thread != null)
+      if (thread != null)
       {
          throw new RuntimeException("Thread has already been scheduled");
       }
       MonotonicTime time = new MonotonicTime(0, TimeUnit.NANOSECONDS.convert(period, timeunit));
       PeriodicParameters periodicParameters = new PeriodicParameters(time);
       thread = new PeriodicRealtimeThread(priorityParameters, periodicParameters, runnable, name);
+      if (processors != null)
+      {
+         thread.setAffinity(processors);
+      }
       thread.start();
    }
 
--- src/main/java/us/ihmc/util/PeriodicRealtimeThreadSchedulerFactory.java
+++ src/main/java/us/ihmc/util/PeriodicRealtimeThreadSchedulerFactory.java
@@ -1,33 +1,40 @@
 package us.ihmc.util;
 
+import us.ihmc.affinity.Processor;
 import us.ihmc.realtime.PriorityParameters;
-import us.ihmc.util.PeriodicRealtimeThreadScheduler;
-import us.ihmc.util.PeriodicThreadScheduler;
 
 /**
  * Factory to create multiple instances of PeriodicRealtimeThreadScheduler
  * 
  * @author Jesper Smith
- *
  */
 public class PeriodicRealtimeThreadSchedulerFactory implements PeriodicThreadSchedulerFactory
 {
    private final PriorityParameters priority;
+   private final Processor[] processors;
 
    public PeriodicRealtimeThreadSchedulerFactory(PriorityParameters priority)
    {
       this.priority = priority;
+      this.processors = null;
    }
 
    public PeriodicRealtimeThreadSchedulerFactory(int priority)
    {
-      this.priority = new PriorityParameters(priority);
+      this(new PriorityParameters(priority));
+   }
+
+   public PeriodicRealtimeThreadSchedulerFactory(PriorityParameters priority, Processor... processors)
+   {
+      this.priority = priority;
+      this.processors = processors;
    }
 
    @Override
    public PeriodicThreadScheduler createPeriodicThreadScheduler(String name)
    {
-      return new PeriodicRealtimeThreadScheduler(name, priority);
+      PeriodicRealtimeThreadScheduler periodicRealtimeThreadScheduler = new PeriodicRealtimeThreadScheduler(name, priority, processors);
+      return periodicRealtimeThreadScheduler;
    }
 
 }
